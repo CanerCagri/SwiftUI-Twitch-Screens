3e5334d5c941e6e334ef8adc1eb8268c63b5d96d
CanerCagri <canerrcagrii@gmail.com>
2023-06-30 14:35:49 +0300

    Initial Commit

---
+++ SwiftUI-Twitch-Screens/SwiftUI_Twitch_ScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Twitch_ScreensApp.swift
+//  SwiftUI-Twitch-Screens
+//
+//  Created by Caner Çağrı on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Twitch_ScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
